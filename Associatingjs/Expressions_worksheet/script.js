51e56227ccadfa9b3627f29c6bcc1bc56f2794f3
clarence Willey <blaqmat1c@fullsail.edu>
2013-05-16 20:24:49 -0700

    adding all problems

---
+++ Associatingjs/Expressions_worksheet/script.js
@@ -0,0 +1,81 @@
+ // Clarence Willey, 05/15/2013 Expression Worksheet
+
+ // Calculate Sparky the pit bulls age in dog years based on his actual age
+var age;
+
+var age = 13;
+
+var ageDogYears;
+
+var ageDogYears = age * 7;   //Multiply the dogs real age * 7 to get age in dog years
+
+console.log(ageDogYears);
+
+
+// how many slices of pizza per person//
+
+var numberOfSlices;
+
+numberOfSlices = 8;
+
+var numberOfPeopleAtTheParty;
+
+ numberOfPeopleAtTheParty = 9;
+
+var numberPizzasOrdered;
+
+ numberPizzasOrdered = 4;
+
+var numberSlicesPerPerson;
+
+ numberSlicesPerPerson = numberOfSlices * numberPizzasOrdered / numberOfPeopleAtTheParty;
+
+console.log(numberSlicesPerPerson);
+
+
+
+ // number of slices sparky gets
+
+ var sparkySlices = 32 % 9;
+ console.log(sparkySlices);
+
+
+ // find average shopping total
+
+ var weeklyFoodBills = [ 115.00, 75.00, 97.00, 125.00, 94.00];
+ var total = weeklyFoodBills[0]  + weeklyFoodBills[1] +   weeklyFoodBills[2] + weeklyFoodBills[3] + weeklyFoodBills[4];
+ var average = total / 5;
+ console.log(total, average); // this displays the total first then the average
+
+// discount price of shoes
+
+var price;
+price = 160.00; // original price of the Air Jordan's
+var discount;
+discount = price * .20;  // amount of discount
+ var newPrice;
+ newPrice = price - discount;  // price after discount with no tax added
+var tax;
+ tax = .05;  // tax rate
+ var salesTax;
+  salesTax = newPrice * tax; // dollar amt of tax
+var priceWTax;
+priceWTax = newPrice + salesTax;   // price with tax added
+var item;
+ item = "NIKE AIR JORDAN RETRO 6 sneakers";
+ var answer;
+  answer = "Your";
+ var answer2;
+ answer2 = "were orignally";
+ var answer3;
+ answer3 = "but after 20% off they are now";
+ var answer5;
+ answer5 = "without tax, and";
+ var answer6;
+ answer6 = "with tax.";
+
+ console.log(answer, item, answer2, price, answer3,  newPrice, answer5,  priceWTax, answer6);
+
+
+
+
---
+++ Expressions_worksheet/script.js
@@ -0,0 +1,37 @@
+ // Clarence Willey, 05/15/2013 Expression Worksheet
+
+ // Calculate Sparky the pit bulls age in dog years based on his actual age
+var age;
+
+var age = 2;
+
+var ageDogYears;
+
+var ageDogYears = age *7;   //Multiply the dogs real age * 7 to get age in dog years
+
+console.log(ageDogYears);
+
+
+// how many slices of pizza per person//
+
+var numberOfSlices;
+
+numberOfSlices = 8;
+
+var numberOfPeopleAtTheParty;
+
+ numberOfPeopleAtTheParty = 9;
+
+var numberPizzasOrdered;
+
+ numberPizzasOrdered = 4;
+
+var numberSlicesPerPerson;
+
+ numberSlicesPerPerson = numberOfSlices * numberPizzasOrdered / numberOfPeopleAtTheParty;
+
+console.log(numberSlicesPerPerson);
+
+
+
+
