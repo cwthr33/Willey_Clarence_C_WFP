a35723fd8cefa0cea2b5db4bf85c0c7dac8bed1b
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-23 23:09:03 -0400

    added  correct punctuation

--- Expressions_Personal/Personal/script1.js
+++ Expressions_Personal/Personal/script1.js
@@ -2,7 +2,7 @@
 
 var days = prompt('Brush Calculator to see how many times a year you brush your teeth \n Enter how may times a day you brush your teeth');  // collecting first var for equation
 
-var week = prompt('How many days a week do you brush your teeth'); // collecting the second var
+var week = prompt('How many days a week do you brush your teeth?'); // collecting the second var
 
 
 
