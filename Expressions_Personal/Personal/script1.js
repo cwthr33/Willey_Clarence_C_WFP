77378fe7dc90780d5d09f9ed771c106a31774132
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-23 23:14:23 -0400

    fixed error
    left off semi colon on last var

--- Expressions_Personal/Personal/script1.js
+++ Expressions_Personal/Personal/script1.js
@@ -4,7 +4,9 @@ var days = prompt('Brush Calculator to see how many times a year you brush your 
 
 var week = prompt('How many days a week do you brush your teeth?'); // collecting the second var
 
-var month = prompt('How many months out of the year do you brush your teeth? \n Please enter a number 1 - 12.')  // last variable
+var month = prompt('How many months out of the year do you brush your teeth? \n Please enter a number 1 - 12.');  // last variable
+
+var result = days *
 
 
 
