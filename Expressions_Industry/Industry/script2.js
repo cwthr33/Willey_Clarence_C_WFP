b2c8e356bb7897189d25a98ca6611c4ce3dcf658
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-24 00:20:46 -0400

    industry expression

--- Expressions_Industry/Industry/script2.js
+++ Expressions_Industry/Industry/script2.js
@@ -1,7 +1,12 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Ari
- * Date: 5/23/13
- * Time: 11:34 PM
- * To change this template use File | Settings | File Templates.
- */
+// Clarence Willey Expressions
+
+var daysWeek = prompt('Vacation Calculator \n Enter the number of days you worked this week? \n Enter a number 1 - 7.');  // collecting first var for equation for each day worked you will earn .5 hours
+
+var weeks = prompt('How many weeks in this month? \n Enter a number 1 - 5.'); //second variable   multiply days worked by weeks to give total days for the month
+
+var lates = prompt('How many times were you late this month? \n Please enter any number.');  // lates will deduct .5 hours in vacation
+
+var result = (daysWeek * weeks) * .50 - (lates * .50);
+
+alert('You will have ' + result + ' hours of vacation this month');
+
