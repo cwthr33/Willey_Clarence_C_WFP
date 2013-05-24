e7992e0a73390e12bc755226e9161fe7fb8b55df
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-24 00:59:05 -0400

    wacky

---
+++ Expressions_Wacky/Wacky/script3.js
@@ -0,0 +1,11 @@
+// Clarence Willey Expressions
+
+var name = prompt('Please enter your first name');  // collecting first var for equation
+
+var middle = prompt('Please enter your middle name?'); // collecting the second var
+
+var last = prompt('Please enter your last name');  // last variable
+
+var result = name  +  middle   +   last;
+
+alert( result + 'You are a SEXY BEAST! Yeah Baby, Yeah!');
