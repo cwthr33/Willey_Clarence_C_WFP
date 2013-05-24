afcfdae74374d846d1352aaaa279d9fe5b6d08c6
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-23 23:02:46 -0400

    first var created

--- Expressions_Personal/Personal/script1.js
+++ Expressions_Personal/Personal/script1.js
@@ -1,3 +1,8 @@
-// Clarence Willey Expressions    1
+// Clarence Willey Expressions
+
+var days = prompt('Brush Calculator to see how many times a year you brush your teeth \n Enter how may times a day you brush your teeth');  // collecting first var for equation
 
 
+
+
+console.log(days);
