79056268c9627d28f2854989ff44a205e8cd9545
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-23 23:09:47 -0400

    Corrected punctuation

--- Expressions_Personal/Personal/script1.js
+++ Expressions_Personal/Personal/script1.js
@@ -1,6 +1,6 @@
 // Clarence Willey Expressions
 
-var days = prompt('Brush Calculator to see how many times a year you brush your teeth \n Enter how may times a day you brush your teeth');  // collecting first var for equation
+var days = prompt('Brush Calculator to see how many times a year you brush your teeth \n Enter how may times a day you brush your teeth?');  // collecting first var for equation
 
 var week = prompt('How many days a week do you brush your teeth?'); // collecting the second var
 
