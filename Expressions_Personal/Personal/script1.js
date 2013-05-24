f78ca3ce8926fd8feeb5f919b812a8ae39edd0a9
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-23 23:12:26 -0400

    last var added

--- Expressions_Personal/Personal/script1.js
+++ Expressions_Personal/Personal/script1.js
@@ -4,6 +4,8 @@ var days = prompt('Brush Calculator to see how many times a year you brush your 
 
 var week = prompt('How many days a week do you brush your teeth?'); // collecting the second var
 
+var month = prompt('How many months out of the year do you brush your teeth? \n Please enter a number 1 - 12.')  // last variable
+
 
 
 console.log(days);
