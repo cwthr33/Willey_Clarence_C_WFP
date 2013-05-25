720c1c56bca2e9cf1717fa7f3c21de3bf3205e94
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-24 22:42:22 -0400

    condition

--- Conditionals_Industry/Conditions/script.js
+++ Conditionals_Industry/Conditions/script.js
@@ -1,4 +1,15 @@
- // Clarence Willey, 05/15/2013 Expression Worksheet
+ // Clarence Willey, 05/24/2013 Conditionals
+
+ // I work in a call center so I will use this to calc calls per hr for an agent
+
+ var callsPerHr = function(calls,secsPshift,secsPhr){
+     var callspHr = calls / (secsPshift / secsPhr)
+     return callspHr;
+
+ }
+
+   var stat = callsPerHr(85, 28800, 3600);
+  console.log('Clarence takes  ' + stat + '  calls per hour.')
 
 
 
