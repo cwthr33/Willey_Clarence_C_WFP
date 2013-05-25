f244af9440ca340dfb09e1e0130927b0e6892f06
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-24 22:12:13 -0400

    final commit for personal Conditional

--- Conditionals_Personal/Personal/script1.js
+++ Conditionals_Personal/Personal/script1.js
@@ -4,9 +4,11 @@
      var miles = 2550;  // var input
      var tank = 17;   // var input
      var gallons = tank * fillups;  // formula to figure gallons
-     var mpg = miles / gallons;     // formula to figure miles per gallon
-
+     var mpg = miles/gallons;     // formula to figure miles per gallon
      console.log('You are getting  ' + mpg + '  miles to the gallon.');
+   }
+
+       mpg(2);
 
 
 
@@ -15,5 +17,6 @@
 
 
 
- }
+
+
 
