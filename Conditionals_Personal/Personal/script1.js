6cfd80d69d7ca11eb99cbb2963999095d544898b
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-24 21:06:14 -0400

    var input

--- Conditionals_Personal/Personal/script1.js
+++ Conditionals_Personal/Personal/script1.js
@@ -1,2 +1,14 @@
- // Clarence Willey, 05/15/2013 Expression Worksheet
+ // Clarence Willey, 05/24/2013 Conditional Assignment
+
+ function mpg(fillups){
+     var miles = 2550;  // var input
+
+
+
+
+
+
+
+
+ }
 
