1ffe2e6931fbe9c8d0a166d9375b7da509937cd8
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-24 21:08:58 -0400

    added mpg formula and console log

--- Conditionals_Personal/Personal/script1.js
+++ Conditionals_Personal/Personal/script1.js
@@ -4,7 +4,9 @@
      var miles = 2550;  // var input
      var tank = 17;   // var input
      var gallons = tank * fillups;  // formula to figure gallons
+     var mpg = miles / gallons;     // formula to figure miles per gallon
 
+     console.log('You are getting  ' + mpg + '  miles to the gallon.');
 
 
 
