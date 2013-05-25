e7217425fc04d52c8053c887f0a6b782bc320b59
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-24 23:54:19 -0400

    condition complete

--- Conditionals_Wacky/Wacky/script5.js
+++ Conditionals_Wacky/Wacky/script5.js
@@ -1,7 +1,11 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Ari
- * Date: 5/24/13
- * Time: 11:27 PM
- * To change this template use File | Settings | File Templates.
- */
+// Clarence Willey 5/24/13 Conditionals
+
+  var blueCandy = function(red, yellow, green){
+    var blue = (red * green) / yellow
+           return blue;
+
+}
+
+  var favKind = blueCandy(15, 20, 10);
+
+console.log('My favorite color M&M\'s are the BLUE ones and there are  ' + favKind + '  BLUE M&M\'s in each bag! I always save the half for last.');
