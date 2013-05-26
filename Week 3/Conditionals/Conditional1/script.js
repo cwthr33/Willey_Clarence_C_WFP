742f0080e974b7fd146a63197cbfdb5bc593a414
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-25 22:39:23 -0400

    correction to code to change prompt for formula

--- Week 3/Conditionals/Conditional1/script.js
+++ Week 3/Conditionals/Conditional1/script.js
@@ -1,6 +1,7 @@
   // Clarence Willey Conditionals
 
      var name = prompt('Who do you miss? \n Enter that persons name here '); //<--- getting name to fill var
-     var when = prompt('When was the last time you saw  ' + name + '\n  Please Enter Date Here in ths format MM/DD/YYYY'); // <---- getting date last saw the person from above
+     var when = prompt('How many days since you last saw  ' + name + '\n  Please Enter the number of days here.'); // <---- getting # of days
+
 
 
