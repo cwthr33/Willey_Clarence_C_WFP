0bdfa6de1f278810f0975279fa835e91482f2438
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-25 22:58:32 -0400

    adding last var's and testing code

--- Week 3/Conditionals/Conditional1/script.js
+++ Week 3/Conditionals/Conditional1/script.js
@@ -4,6 +4,10 @@
      var when = prompt('How many days since you last saw  ' + name + '\n  Please Enter the number of days here.'); // <---- getting # of days
      var hours = prompt('How many hours in a day? \n Please enter the number of hours in a standard earth day here.');// <---- collecting number of hours in a day which should be 24
     var min = prompt('How many minutes in an hour? \n Please enter that number here.'); // <--- collecting minutes from the human
+    var answer = when * hours / min;
+
+
+  console.log(answer);
 
 
 
