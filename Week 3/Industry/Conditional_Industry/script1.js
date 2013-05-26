c49a1e1330f2b51e8bd3499afeebe7244fe3a522
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 14:24:42 -0400

    first variable
    calculating pay estimate formula

--- Week 3/Industry/Conditional_Industry/script1.js
+++ Week 3/Industry/Conditional_Industry/script1.js
@@ -1,7 +1,4 @@
-/**
- * Created with JetBrains WebStorm.
- * User: Ari
- * Date: 5/26/13
- * Time: 1:48 PM
- * To change this template use File | Settings | File Templates.
- */
+// Clarence Willey Conditional
+
+
+ var name prompt('Enter your name in the space provided below.');
