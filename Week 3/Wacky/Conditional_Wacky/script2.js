569bfe6cecfc5319cb1ac80356106d31c2b2902e
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 16:42:26 -0400

    second half of ternary

--- Week 3/Wacky/Conditional_Wacky/script2.js
+++ Week 3/Wacky/Conditional_Wacky/script2.js
@@ -5,4 +5,4 @@ var numOne = prompt('How much wood could a Woodchuck chuck?'); // collecting dat
 var numTwo = prompt('How many guitarists does it take to screw in a light bulb?'); // collecting data for code
 var answer = numOne * numTwo; // multiplying number one and number two
 
-(answer > 100) ? console.log('I can only please one person per day. Today isn\'t your day...and tomorrow doesn\'t look good either.') :
+(answer > 100) ? console.log('I can only please one person per day. Today isn\'t your day...and tomorrow doesn\'t look good either.') : console.log('Your weirdness is creeping my imaginary friend out. And if you don\'t stop it...he seriously just might eat you. ');
