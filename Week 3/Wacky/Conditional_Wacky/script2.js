c8e127d6b7a90f0221f5bb9288a71bc29293c1ee
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 16:19:27 -0400

    Second item for code

--- Week 3/Wacky/Conditional_Wacky/script2.js
+++ Week 3/Wacky/Conditional_Wacky/script2.js
@@ -1,3 +1,4 @@
 // Clarence Willey Conditional
 
 var numOne = prompt('How much wood could a Woodchuck chuck?'); // collecting data from person
+var numTwo = prompt('How many guitarists does it take to screw in a light bulb?')
