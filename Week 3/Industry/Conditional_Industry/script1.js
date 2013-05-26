6095af48d73a2cc513ba60a8d5117c99ae1f65cc
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 14:37:36 -0400

    comments

--- Week 3/Industry/Conditional_Industry/script1.js
+++ Week 3/Industry/Conditional_Industry/script1.js
@@ -4,4 +4,4 @@
  var name = prompt('Enter your name in the space provided below.');// collecting the persons name
  var hours = prompt('Please enter your hours worked for this week in the space provided below.');// collecting hours worked
  var pay = prompt('Please enter your hourly rate of pay.\n Example 12.00.');// collecting the persons pay rate
- var total = hours * pay;
+ var total = hours * pay;// here we will calculate the total pay earned
