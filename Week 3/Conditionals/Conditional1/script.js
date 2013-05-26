4aa0e398609863ce303e1391650826ed995ebfc0
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-25 23:00:02 -0400

    corrected spacing issue

--- Week 3/Conditionals/Conditional1/script.js
+++ Week 3/Conditionals/Conditional1/script.js
@@ -1,7 +1,7 @@
   // Clarence Willey Conditionals
 
      var name = prompt('Who do you miss? \n Enter that persons name here '); //<--- getting name to fill var
-     var when = prompt('How many days since you last saw  ' + name + '\n  Please Enter the number of days here.'); // <---- getting # of days
+     var when = prompt('How many days since you last saw ' + name + '\n  Please Enter the number of days here.'); // <---- getting # of days
      var hours = prompt('How many hours in a day? \n Please enter the number of hours in a standard earth day here.');// <---- collecting number of hours in a day which should be 24
     var min = prompt('How many minutes in an hour? \n Please enter that number here.'); // <--- collecting minutes from the human
     var answer = when * hours / min;
