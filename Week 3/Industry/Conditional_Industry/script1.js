1e837e2288d85d958dad1cd7af4b6f5786712914
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 15:57:56 -0400

    moving test code

--- Week 3/Industry/Conditional_Industry/script1.js
+++ Week 3/Industry/Conditional_Industry/script1.js
@@ -6,9 +6,10 @@
  var pay = prompt('Please enter your hourly rate of pay.\n Example 12.00.');// collecting the persons pay rate
  var total = hours * pay; // here we will calculate the total pay earned
 
- console.log(total); // testing code with this
+
 
 (total >= 480)? (name + ' , you will be paid ' + total + ' dollars for your hours worked this week!') : (name + ' , because you worked less than your scheduled 40 hours you will be paid ' + total + ' dollars this week. Please remember attendance is important.');   // deciding what to say if true or false
+
 
 
 
