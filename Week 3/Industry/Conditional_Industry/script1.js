a0ef523ddb0eac7a55cb6ffff4392c337f774eb4
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 14:31:30 -0400

    added var to collect rate of pay

--- Week 3/Industry/Conditional_Industry/script1.js
+++ Week 3/Industry/Conditional_Industry/script1.js
@@ -3,3 +3,4 @@
 
  var name = prompt('Enter your name in the space provided below.');// collecting the persons name
  var hours = prompt('Please enter your hours worked for this week in the space provided below.');// collecting hours worked
+ var pay = prompt('Please enter your hourly rate of pay.\n Example 12.00.');
