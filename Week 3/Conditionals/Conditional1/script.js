e7057874a23089ae72791d15e9526e539d84704c
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 13:39:13 -0400

    comments for console

--- Week 3/Conditionals/Conditional1/script.js
+++ Week 3/Conditionals/Conditional1/script.js
@@ -5,7 +5,7 @@
      var hours = prompt('How many hours in a day? \n Please enter the number of hours in a standard earth day here.');// <---- collecting number of hours in a day which should be 24
     var min = prompt('How many minutes in an hour? \n Please enter that number here.'); // <--- collecting minutes from the human
     var answer = when * hours * min;   // formula to fo math to find the answer
-  console.log(answer);
+  console.log(answer);  // printing the answer to the console
 
    (answer > 4320)? console.log('I miss you so much!') : console.log('I just saw you yesterday!')
 
