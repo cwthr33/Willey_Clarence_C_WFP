768d7ee495c64ce98444aa3b8a40bad7d09da50d
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 16:27:12 -0400

    comments to test code here

--- Week 3/Wacky/Conditional_Wacky/script2.js
+++ Week 3/Wacky/Conditional_Wacky/script2.js
@@ -4,4 +4,4 @@ var name = prompt('Hey help me out and enter your name under here. ');// collect
 var numOne = prompt('How much wood could a Woodchuck chuck?'); // collecting data from person
 var numTwo = prompt('How many guitarists does it take to screw in a light bulb?'); // collecting data for code
 var answer = numOne * numTwo; // multiplying number one and number two
-console.log(answer);
+console.log(answer); // testing code here
