7f0253eb95d6f171cadbc3f8a551d3ebc84adf44
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 16:22:26 -0400

    added code to collect name

--- Week 3/Wacky/Conditional_Wacky/script2.js
+++ Week 3/Wacky/Conditional_Wacky/script2.js
@@ -1,4 +1,5 @@
 // Clarence Willey Conditional
 
+var name = prompt('Hey help me out and enter your name under here. ')
 var numOne = prompt('How much wood could a Woodchuck chuck?'); // collecting data from person
 var numTwo = prompt('How many guitarists does it take to screw in a light bulb?'); // collecting data for code
