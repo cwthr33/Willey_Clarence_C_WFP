af8d70db3877b27dfad5998600bd3f02ff8785e6
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-26 14:28:54 -0400

    added comments

--- Week 3/Industry/Conditional_Industry/script1.js
+++ Week 3/Industry/Conditional_Industry/script1.js
@@ -2,4 +2,4 @@
 
 
  var name = prompt('Enter your name in the space provided below.');// collecting the persons name
- var hours = prompt('Please enter your hours worked for this week in the space provided below.');
+ var hours = prompt('Please enter your hours worked for this week in the space provided below.');// collecting hours worked
