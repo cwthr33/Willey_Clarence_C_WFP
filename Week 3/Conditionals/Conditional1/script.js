66e2457f5de9b93abb15cd05956b2c95305b31e5
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-25 22:42:23 -0400

    comments and adding new prompt to code

--- Week 3/Conditionals/Conditional1/script.js
+++ Week 3/Conditionals/Conditional1/script.js
@@ -2,6 +2,6 @@
 
      var name = prompt('Who do you miss? \n Enter that persons name here '); //<--- getting name to fill var
      var when = prompt('How many days since you last saw  ' + name + '\n  Please Enter the number of days here.'); // <---- getting # of days
-
+     var hours = prompt('How many hours in a day? \n Please enter the number of hours in a standard earth day here.');// <---- collecting number of hours in a day which should be 24
 
 
