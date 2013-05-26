34de7f25bb7035478f6e033898b8cf9eebceb3c8
Clarence Willey <blaqmat1c@fullsail.edu>
2013-05-25 22:23:24 -0400

    second var collected here

--- Week 3/Conditionals/Conditional1/script.js
+++ Week 3/Conditionals/Conditional1/script.js
@@ -1,4 +1,5 @@
   // Clarence Willey Conditionals
 
-     var name = Prompt('Who do you miss? \n Enter that persons name here ');
+     var name = prompt('Who do you miss? \n Enter that persons name here '); //<--- getting name to fill var
+     var when = prompt('When was the last time you saw' + name);
 
